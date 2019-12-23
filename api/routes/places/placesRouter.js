1d72c78bf7e7657a22c58cfb6881830cc1fe1886
ndurbin <nick.durbin@gmail.com>
2019-12-22 20:36:15 -0500

    Setup the boilerplate for my palces router.

---
+++ api/routes/places/placesRouter.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const db = require('../../../data/dbConfig')
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+  try {
+
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/:id', (req, res, next) => {
+  try {
+
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/', (req, res, next) => {
+  try {
+
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:id', (req, res, next) => {
+  try {
+
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:id', (req, res, next) => {
+  try {
+
+  } catch (error) {
+    next(error)
+  }
+})
+
+
+module.exports = router
