e9273662ae3f6d201aa34691a89cb42386d7dee6
ndurbin <nick.durbin@gmail.com>
2019-12-22 20:35:31 -0500

    Created/setup my server file for use.

---
+++ api/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const server = express()
+const routes = require('./routes')
+const middleware = require('./middleware')
+
+server.use(express.json())
+middleware(server)
+routes(server)
+
+server.get("/", (req, res, next) => {
+  res.send("<h2>I am your server.</h2>")
+})
+
+server.use((req, res, next) => {
+  res.status(404).json({ message: "You have wandered too far. Go back now."})
+})
+
+server.use((err, req, res, next) => {
+  res.status(500).json({ message: "There is an issue with the server. Our bad."})
+})
+
+module.exports = server
