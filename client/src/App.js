a6297d705c3d55788693bc1b64d3bd6ac68ad636
ndurbin <nick.durbin@gmail.com>
2019-12-23 02:45:41 -0500

    Displaying the data below the map.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { GoogleMap, withScriptjs, withGoogleMap, Marker } from 'react-google-maps';
+import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
+import axios from 'axios'
 
 function Map() {
   return <GoogleMap defaultZoom={10} defaultCenter={{ lat: 41.081444, lng: -81.519005 }} />
@@ -9,6 +10,18 @@ const WrappedMap = withScriptjs(withGoogleMap(Map))
 
 
 function App() {
+  const [places, setPlaces] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/api/places')
+      .then(res => {
+        setPlaces(res.data)
+      })
+      .catch(err => {
+        console.log(err, 'There was an issue catching the data.')
+      })
+  }, [])
+
   return (
     <div className="App">
       <WrappedMap 
@@ -17,6 +30,17 @@ function App() {
       loadingElement={<div style={{ height: `100%` }} />}
       containerElement={<div style={{ height: `400px` }} />}
       mapElement={<div style={{ height: `100%` }} />} />
+
+      {places.map(place => {
+        return (
+          <div className='placeContainer' key={place.id}>
+            <h2>Name: {place.name}</h2>
+            <h3>Address: {place.address} Zipcode:{place.zipcode}</h3>
+            <h3>Website: {place.website}</h3>
+          </div>
+        )
+      })}
+
     </div>
   );
 }
