971e839d02030dc4f92e62c0e2e808b8d5f84120
nick.durbin <nick.durbin@gmail.com>
2020-01-05 14:19:02 -0500

    Working on getting a dynamically rendered center.

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,8 @@ import { GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow } from 'reac
 import axios from 'axios';
 import AutoCompleteForm from './components/AutoCompleteForm';
 
-function Map() {
+function Map({ selectedPlace, setSelectedPlace }) {
   const [places, setPlaces] = useState([])
-  const [selectedPlace, setSelectedPlace] = useState(null)
 
   useEffect(() => {
     axios.get('http://localhost:4000/api/places')
@@ -19,7 +18,7 @@ function Map() {
 
   return <GoogleMap 
           defaultZoom={10} 
-          defaultCenter={{ lat: 41.081444, lng: -81.519005 }}>
+          defaultCenter={{lat: null, lng: null} ? { lat: 41.081444, lng: -81.519005 } : {lat: selectedPlace.lat, lng: selectedPlace.long }}>
 
           {places.map(place => {
             return (
@@ -47,7 +46,8 @@ function Map() {
 const WrappedMap = withScriptjs(withGoogleMap(Map))
 
 function App() {
-  
+  const [selectedPlace, setSelectedPlace] = useState(null)
+
   return (
     <div className="App">
       <WrappedMap 
@@ -55,9 +55,11 @@ function App() {
       googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_API_KEY}`}
       loadingElement={<div style={{ height: `100%` }} />}
       containerElement={<div style={{ height: `400px` }} />}
-      mapElement={<div style={{ height: `100%` }} />} />
+      mapElement={<div style={{ height: `100%` }} />} 
+      setSelectedPlace={setSelectedPlace} 
+      selectedPlace={selectedPlace} />
 
-    <AutoCompleteForm />
+    <AutoCompleteForm setSelectedPlace={setSelectedPlace} selectedPlace={selectedPlace}/>
 
     </div>
   );
